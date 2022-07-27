26d7f2ac582aa35b8ec9c2e2ea42d37601c4e42e
Trinitrogen <kbellgra@gmail.com>
2022-07-27 08:56:47 -0500

    added .gitignore

---
+++ NodeMCU-Thermometer/arduino_secrets.h
@@ -0,0 +1,10 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define WIFI_SSID  "My-Wifi-SSID"
+#define WIFI_PASS  "My-Wifi-Password"
+#define MQTT_SERVER "MQTT Server Hostname"
+#define MQTT_USER "Username for MQTT Server"
+#define MQTT_PASS "Password for MQTT Server"
+
+#endif
