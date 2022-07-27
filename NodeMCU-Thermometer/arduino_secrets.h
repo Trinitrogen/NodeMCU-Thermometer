fdb66b4cad1b48fda21c4e99fa24f0517d7a7fd4
Trinitrogen <kbellgra@gmail.com>
2022-07-27 09:05:36 -0500

    Fixing arduino_secrets.h

--- NodeMCU-Thermometer/arduino_secrets.h
+++ NodeMCU-Thermometer/arduino_secrets.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef SECRETS_H
 #define SECRETS_H
 
@@ -9,10 +8,3 @@
 #define MQTT_PASS "Password for MQTT Server"
 
 #endif
-=======
-#define WIFI_SSID "My Wifi SSID"
-#define WIFI_PASS "My Wifi Password"
-#define MQTT_SERVER "MQTT Server Hostname"
-#define MQTT_USER "Username for MQTT Server"
-#define MQTT_PASS "Password for MQTT Server"
->>>>>>> e2d3200c751638a458fad0d7d8c0754fee8f1f88
