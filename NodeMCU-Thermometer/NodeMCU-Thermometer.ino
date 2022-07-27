d6ea3a13aa7d51c4dae836d930a44ebeddbc9239
Trinitrogen <kbellgra@gmail.com>
2022-07-27 08:25:51 -0500

    Initial Commit

---
+++ NodeMCU-Thermometer/NodeMCU-Thermometer.ino
@@ -0,0 +1,19 @@
+/*
+NodeMCU-Thermometer
+
+*/
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include "DHT.h"
+#include <arduino_secrets.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
